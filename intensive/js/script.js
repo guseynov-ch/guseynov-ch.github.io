50750e3aa5b8a8c441df3dbfc5f7488873b4f9d7
Chingiz <chingiz_guseynov@list.ru>
2020-10-23 18:54:16 +0300

    corrected the folders4

---
+++ intensive/js/script.js
@@ -0,0 +1,55 @@
+// hamburger
+window.addEventListener('DOMContentLoaded', () => {
+	const menu = document.querySelector('.main-menu__menu'),
+		menuItem = document.querySelectorAll('.main-menu__item'),
+		hamburger = document.querySelector('.hamburger');
+
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger_active');
+		menu.classList.toggle('main-menu_active');
+	});
+
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('main-menu_active');
+		});
+	});
+});
+
+// sick slider
+$(document).ready(function () {
+	$('.carousel').slick({
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		centerMode: true,
+		centerPadding: '10px',
+		slidesToShow: 3,
+		speed: 300,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prev.svg"></img></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.svg"></img></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					arrows: false,
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 575,
+				settings: {
+					arrows: false,
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
